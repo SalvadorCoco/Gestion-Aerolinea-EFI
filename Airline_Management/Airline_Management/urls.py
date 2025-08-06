3c64d1aea6b12791e852eaf1a087656a0f7f28f9
Salvador Coco <salvadorcocosanchez@gmail.com>
2025-08-06 09:03:55 -0300

    fix gitignore y añadi los pasajeros a la home

--- Airline_Management/Airline_Management/urls.py
+++ Airline_Management/Airline_Management/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('airplanes/', include('apps.airplanes.urls')),
+    path('passengers/', include('apps.passengers.urls')),
     path('', include('apps.accounts.urls')),
 ]
