baae9f3e0148fff0b8d579f4bcebea090179ee78
tomighersi <tomigh717@gmail.com>
2025-08-14 09:36:43 -0300

    fix conflict

---
+++ Airline_Management/apps/reservations/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    ReservationCreate,
+    ReservationListView,
+    ReservationUpdateView,
+    ReservationDeleteView,
+    ReservationDetailView,
+)
+
+urlpatterns = [
+    path('', ReservationListView.as_view(), name='list'),
+    path('create/', ReservationCreate.as_view(), name='create'),
+    path('<int:pk>/update/', ReservationUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', ReservationDeleteView.as_view(), name='delete'),
+    path('<int:pk>/', ReservationDetailView.as_view(), name='detail'),
+    ]
