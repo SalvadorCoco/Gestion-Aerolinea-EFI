c092e6be319b508b202d9a7af6db3143bb7e63a8
tomighersi <tomigh717@gmail.com>
2025-08-14 09:14:30 -0300

    passenger listo

--- Airline_Management/apps/passengers/forms.py
+++ Airline_Management/apps/passengers/forms.py
@@ -5,7 +5,28 @@ from .models import Passenger
 class PassengerForm(forms.ModelForm):
     class Meta:
         model = Passenger
-        fields = '__all__'
+        fields = ['name', 'document', 'email', 'phone', 'date_birth', 'document_type']
         widgets = {
             'date_birth': forms.DateInput(attrs={'type': 'date'}),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el nombre',
+            }),
+            'document': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el documento',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el email',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el telefono',
+            }),
+            'document_type': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el tipo de documento',
+            }),
+
         }
